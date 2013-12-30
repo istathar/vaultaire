da38d6f00105a34dc64770b75381b304c6c357a3
Andrew Cowie <andrew@operationaldynamics.com>
2013-12-30 13:50:23 +1100

    Begin adding test for serializing Point to vault

--- tests/TestSuite.hs
+++ tests/TestSuite.hs
@@ -39,6 +39,7 @@ import Debug.Trace
 import Vaultaire.Conversion.Receiver
 import Vaultaire.Conversion.Writer
 import qualified Vaultaire.Internal.CoreTypes as Core
+import Vaultaire.Serialize.DiskFormat (Quantity(..),Compression(..))
 import qualified Vaultaire.Serialize.DiskFormat as Disk
 import qualified Vaultaire.Serialize.WireFormat as Protobuf
 
@@ -50,6 +51,11 @@ suite = do
 
     describe "on-disk VaultPrefix" $ do
         testSerializeVaultHeader
+        testRoundTripVaultHeader
+
+    describe "a VaultPoint protobuf" $ do
+        testSerializeVaultPoint
+
 
 
 testSerializeDataFrame =
@@ -110,27 +116,22 @@ testConvertPoint =
 -}
 
 
-testSerializeVaultHeader = do
+testSerializeVaultHeader =
+  let
+    h1 = Disk.VaultPrefix {
+                Disk.version = 7,
+                Disk.compression = Disk.Compressed,
+                Disk.quantity = Disk.Multiple,
+                Disk.size = 42
+            }
+  in do
     it "serializes to the correct bytes" $ do
-        let h = Disk.VaultPrefix {
-                    Disk.version = 7,
-                    Disk.compression = Disk.Compressed,
-                    Disk.quantity = Disk.Multiple,
-                    Disk.size = 42
-                }
-        let h' = encode h
+        let h' = encode h1
 
         assertEqual "Incorrect number of bytes" 2 (S.length h')
         assertEqual "Incorrect serialization" [0x7c,0x2a] (S.unpack h')
 
     it "deserializes to the correct object" $ do
-        let h1 = Disk.VaultPrefix {
-                    Disk.version = 7,
-                    Disk.compression = Disk.Compressed,
-                    Disk.quantity = Disk.Multiple,
-                    Disk.size = 42
-                }
-
         let h' = S.pack [0x7c,0x2a]
 
         let eh2 = decode h'
@@ -138,4 +139,51 @@ testSerializeVaultHeader = do
         case eh2 of
             Left err    -> assertFailure err
             Right h2    -> assertEqual "Incorrect deserialization" h1 h2
+
+
+testRoundTripVaultHeader =
+  let
+    h1  = Disk.VaultPrefix 7 Compressed Multiple 42
+    h1' = S.pack [0x7a,0x2a]
+
+    h2  = Disk.VaultPrefix 0 Normal Single 65535
+    h2' = S.pack [0x01,0xff,0xff]
+  in do
+    it "round-trips correctly at boundaries" $ do
+        pendingWith "Waiting on QuickCheck"
+
+
+
+testSerializeVaultPoint =
+  let
+    tags = Map.fromList
+           [("hostname", "secure.example.org"),
+            ("metric", "eth0-tx-bytes"),
+            ("datacenter", "lhr1"),
+            ("epoch", "1")]
+
+    p1 = Core.Point {
+        Core.source = tags,
+        Core.timestamp = 1386931666289201468,
+        Core.payload = Core.Numeric 201468
+--      payload = Core.Textual "203.0.113.101 - - [12/Dec/2013:04:11:16 +1100] \"GET /the-politics-of-praise-william-w-young-iii/prod9780754656463.html HTTP/1.1\" 200 15695 \"-\" \"Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)\""
+--      payload = Core.Measurement 45.9
+    }
+  in
+    it "serializes Core.Point to Disk.VaultPoint " $ do
+        let pb1 = createDiskPoint p1
+        let p1' = runPut $ encodeMessage pb1
+
+        assertEqual "Incorrect length" 13 (S.length p1')
+
+        let epb2 = runGet decodeMessage p1'
+        case epb2 of
+            Left err    -> assertFailure err
+            Right pb2   -> do
+                assertEqual "Incorrect de-serialization" pb1 pb2
+
+                let p2 = undefined
+
+                assertEqual "Point object converted not equal to original object" p1 p2
+
 
