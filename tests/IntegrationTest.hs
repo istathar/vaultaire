e353fe3d7ec4d8ea59d7941cffda1de2556d3fce
Andrew Cowie <andrew@operationaldynamics.com>
2014-07-23 13:18:31 +1000

    Cleanup test pool; register origin

--- tests/IntegrationTest.hs
+++ tests/IntegrationTest.hs
@@ -17,49 +17,73 @@ module Main where
 
 
 import Control.Concurrent
+import qualified Data.ByteString.Char8 as S
 import Data.HashMap.Strict (fromList)
 import Data.Text
 import Pipes
 import qualified Pipes.Prelude as P
 import Test.Hspec hiding (pending)
 
+import CommandRunners
 import DaemonRunners
+import Vaultaire.Daemon
 import Marquise.Client
+import TestHelpers (cleanup)
 
-startDaemons :: IO ()
-startDaemons =
+pool :: String
+pool = "test"
+
+user :: String
+user = "vaultaire"
+
+destroyExistingVault :: IO ()
+destroyExistingVault =
+    runDaemon "inproc://1" (Just (S.pack user)) (S.pack pool) cleanup
+
+startServerDaemons :: MVar () -> IO ()
+startServerDaemons shutdown =
   let
-    pool   = "vaultaire"
-    user   = "vaultaire"
     broker = "localhost"
     bucket_size = 4194304
+    num_buckets = 128
+    step_size = 1440 * 1000000000
     origin = Origin "ZZZZZZ"
     namespace = "integration"
   in do
-    shutdown <- newEmptyMVar
     runBrokerDaemon shutdown
     runWriterDaemon pool user broker bucket_size shutdown
     runReaderDaemon pool user broker shutdown
     runContentsDaemon pool user broker shutdown
     runMarquiseDaemon broker origin namespace shutdown
+    junk <- newEmptyMVar -- FIXME get the MVar out of commands
+    runRegisterOrigin pool user origin num_buckets step_size 0 0 junk
 
 setupClientSide :: IO SpoolFiles
 setupClientSide = do
     createSpoolFiles "integration"
 
+
 main :: IO ()
 main = do
-    startDaemons
+    quit <- newEmptyMVar
+
+    destroyExistingVault
+    startServerDaemons quit
+
     spool <- setupClientSide
+
     hspec (suite spool)
+
+    putMVar quit ()
+
 
 suite :: SpoolFiles -> Spec
 suite spool =
   let
     origin    = Origin "ZZZZZZ"
     address   = hashIdentifier "Row row row yer boat"
-    begin     = 1406078299651575183
-    end       = 1406078299651575183
+    begin     = 1406078299651575182
+    end       = 1406078299651575184
     timestamp = 1406078299651575183
     payload   = 42
   in do
